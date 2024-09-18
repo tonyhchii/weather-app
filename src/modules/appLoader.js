044b0c60c32f48bf3620ef83a48aa6e79c1fbe47
Tony Huang <170560060+tonyhchii@users.noreply.github.com>
2024-09-18 18:33:37 -0400

    Added unit button functionality

--- src/modules/appLoader.js
+++ src/modules/appLoader.js
@@ -1,9 +1,11 @@
 import { loadWeatherObject } from './APIloader';
 import { loadPage } from './DOM/weatherDOM';
 import { loadSearch } from './searchBarController';
+import { loadButtons } from './unitBtnController';
 
 export const loadApp = async () => {
     loadPage(await loadWeatherObject());
     const page = loadSearch();
     page.create();
+    loadButtons();
 }
--- src/modules/unitBtnController.js
+++ src/modules/unitBtnController.js
@@ -1,5 +1,18 @@
-import { setUnit } from "./APIloader";
+import { loadWeatherObject, setUnit } from "./APIloader";
+import { loadPage } from "./DOM/weatherDOM";
 
-const loadButtons = () => {
-    
+const buttons = document.querySelectorAll('.button-container button')
+
+export const loadButtons = () => {
+    buttons.forEach(button => {
+        button.addEventListener('click', async() => {
+            const unit = button.dataset.unitId;
+            setUnit(unit);
+            try {
+                loadPage(await loadWeatherObject());
+            } catch {
+                console.error('Unit Error');
+            }
+        })
+    });
 }
